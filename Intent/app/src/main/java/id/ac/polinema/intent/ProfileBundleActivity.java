551f072ffcab3faad2a43caba319ec0092970bd2
RizkiSaputraWibisono <rizkisaputra329@gmail.com>
2019-09-27 10:39:35 +0700

    membuat package dan class baru, melengkapi source Explicit, Implicit, Android Bundle dan Parcelable

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -2,20 +2,44 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            String value1;
+            String value2;
+            int value3;
+            Bundle bundle = getIntent().getExtras();
+            value1 = bundle.getString("username");
+            value2 = bundle.getString("name");
+            value3 = bundle.getInt("age");
+
+            usernameText.setText(value1);
+            nameText.setText(value2);
+            ageText.setText(String.valueOf(value3));
+
         }
     }
+
+
 }
